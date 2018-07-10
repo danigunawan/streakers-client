74279e432f0d8d69ba6834abd8b4fdae29d5c8ea
misraelson <misraelson@gmail.com>
2018-07-10 16:58:44 -0700

    React client-side for displaying from Rails Streakers API backend. Right now users can be successfully created and json string returning email, password and authentication_token

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import UserSignupForm from "./components/UserSignupForm"
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <UserSignupForm/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/UserSignupForm.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import { Form, Input, Label, Button } from "reactstrap";
+import axios from "axios";
+
+export default class UserSignupForm extends Component {
+  state = {
+      name: "",
+      email: "",
+      password: ""
+  };
+
+  handleChange = event => {
+    const newUser = {};
+    newUser[event.target.name] = event.target.value;
+    this.setState(newUser);
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:3001/v1/users", { user: this.state })
+      .then(function(res) {
+        console.log(res);
+        if (!res.data.errmsg) {
+          localStorage.setItem('accessToken', res.data.user.authentication_token)
+          alert("Thanks for signing up!!");
+        } else {
+          alert("email already used");
+        }
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  };
+
+  render() {
+    return (
+      <Form className="UserForm" onSubmit={this.handleSubmit}>
+        <Label>Name</Label>
+        <Input
+          type="text"
+          name="name"
+          onChange={this.handleChange}
+          value={this.state.name}
+        />
+        <Label>Email</Label>
+        <Input
+          type="text"
+          name="email"
+          onChange={this.handleChange}
+          value={this.state.email}
+        />
+        <Label>Password</Label>
+        <Input
+          type="password"
+          name="password"
+          onChange={this.handleChange}
+          value={this.state.password}
+        />
+        <br />
+        <Button type="submit">Sign Up</Button>
+      </Form>
+    );
+  }
+}
