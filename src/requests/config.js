8aaa3f8f13680921f858f1c3329fddc3e02a3f9c
misraelson <misraelson@gmail.com>
2018-09-05 22:17:55 -0700

    config for deploy

--- src/requests/config.js
+++ src/requests/config.js
@@ -2,8 +2,8 @@
 export const HOST = "localhost:3001";
 export const API_BASE = "/v1";
 // For use in Development 👇
-export const BASE_URL = `http://${HOST}${API_BASE}`;
+// export const BASE_URL = `http://${HOST}${API_BASE}`;
 // For use in Production  👇
-// export const BASE_URL = "/v1";
+export const BASE_URL = "/v1";
 
 // export { HOST, API_BASE, BASE_URL };
