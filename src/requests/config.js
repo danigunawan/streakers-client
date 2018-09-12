6f23970d9b2c71e6f057f71dd6883c54b65d1848
misraelson <misraelson@gmail.com>
2018-09-11 18:08:42 -0700

    no significant change

--- src/requests/config.js
+++ src/requests/config.js
@@ -1,8 +1,9 @@
-// import { HOST, BASE_URL } from ".../requests/config";
 export const HOST = "localhost:3001";
 export const API_BASE = "/v1";
+
 // For use in Development 👇
 // export const BASE_URL = `http://${HOST}${API_BASE}`;
+
 // For use in Production  👇
 export const BASE_URL = "/v1";
 
